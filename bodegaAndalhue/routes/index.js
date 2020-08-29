7c3c06db06c0f73cf2369e640586cc7edbf571a5
Juan Rodríguez <juanviali@gmail.comgit config --global user.email juanviali@gmail.comgit config --global user.email juanviali@gmail>
2020-08-29 18:43:55 -0300

    Crear estructura archivos c/NODE + Express

---
+++ bodegaAndalhue/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/productDetail', function(req, res, next) {
+  res.render('productDetail');
+});
+
+router.get('/productAdd', function(req, res, next) {
+  res.render('productDetail');
+});
+
+router.get('/productCart', function(req, res, next) {
+  res.render('productCart');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+module.exports = router;
