1de6ac3eb5e41ad41d119b545ae65962af2e2b84
EmiJuncos <emilio.juncos1988@gmail.com>
2020-10-03 19:00:00 -0300

    Logica login y register

--- bodegaAndalhue/controllers/usersController.js
+++ bodegaAndalhue/controllers/usersController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { check, validationResult, body } = require('express-validator');
 const fs = require('fs');
 const users = JSON.parse(fs.readFileSync(__dirname + "/../data/users.json"));
@@ -17,6 +18,7 @@ const usersController = {
             res.render("users/register", { errors: errors.errors })
         }
         let newUser = req.body;
+        newUser.password = bcrypt.hashSync(newUser.password, 10);
         if (users.length > 0) {
             let ultimoID = users[users.length - 1].id;
             newUser.id = ultimoID + 1;
@@ -33,8 +35,29 @@ const usersController = {
     },
 
     enter: (req, res, next) => {
-        res.render('users/register');
-    },
+        let errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.render("users/login", { errors: errors.errors })
+        }
+        let userFind;
+        for (var i = 0; i < users.length; i++) {
+
+            if (users[i].email == req.body.email) {
+                if (bcrypt.compareSync(req.body.password, users[i].password)) {
+                    userFind = users[i];
+                    break;
+                }
+
+            }
+        }
+        if (userFind) {
+
+            res.render('users/login');
+        } else {
+            res.render("users/login", { errorAlLoguear: "Usuario o contraseña inválidos!" });
+        }
+    }
 }
 
 module.exports = usersController;
