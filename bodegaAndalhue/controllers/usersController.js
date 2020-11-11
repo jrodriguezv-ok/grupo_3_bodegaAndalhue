e98442c6071f09f28dc084a1ddf27c84a712c085
Marinitabln <marivic_05@hotmail.com>
2020-11-10 22:21:35 -0300

    user controllers

--- bodegaAndalhue/controllers/usersController.js
+++ bodegaAndalhue/controllers/usersController.js
@@ -1,8 +1,6 @@
 const bcrypt = require('bcrypt');
 const { check, validationResult, body } = require('express-validator');
-
-/* const fs = require('fs');
-const users = JSON.parse(fs.readFileSync(__dirname + "/../data/users.json")); */
+const db = require('../database/models');
 
 const usersController = {
     loginRegister: (req, res, next) => {
@@ -17,17 +15,19 @@ const usersController = {
         if (!errors.isEmpty()) {
             res.render("users/register", { errors: errors.errors })
         }
-        let newUser = req.body;
-        newUser.password = bcrypt.hashSync(newUser.password, 10);
-        if (users.length > 0) {
-            let ultimoID = users[users.length - 1].id;
-            newUser.id = ultimoID + 1;
-        } else {
-            newUser.id = 1;
-        }
-        users.push(newUser);
-        /*  fs.writeFileSync(__dirname + "/../data/users.json", JSON.stringify(users)); */
-        res.render('users/register');
+        db.User.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            first_name: req.body.first_name,
+            birthdate: req.body.birthdate,
+            address: req.body.address,
+            town: req.body.town,
+            country: req.body.country,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        User.password = bcrypt.hashSync(User.password, 10);
+        res.render('users/register'); //ver cómo redirigir a la vista anterior: tienda o carrito
     },
 
     login: (req, res, next) => {
@@ -38,19 +38,20 @@ const usersController = {
         let errors = validationResult(req);
         if (!errors.isEmpty()) {
             res.render("users/login", { errors: errors.errors })
-        }
+        };
         let userFind;
-        for (var i = 0; i < users.length; i++) {
-            if (users[i].email == req.body.email) {
-                if (bcrypt.compareSync(req.body.password, users[i].password)) {
-                    userFind = users[i];
-                    break;
+        db.User.findByPk(req.body.email, {
+                include: [{ association: "carts" }]
+            })
+            .then(function(user) {
+                if (user.email == req.body.email) {
+                    if (bcrypt.compareSync(req.body.password, user.password)) {
+                        userFind = user;
+                    }
                 }
-            }
-        }
+            })
         if (userFind) {
             res.render('index', { userLogueado: userFind.first_name });
-
         } else {
             res.render("users/login", { errorAlLoguear: "Usuario o contraseña inválidos!" });
         }
